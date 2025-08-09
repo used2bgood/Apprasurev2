d09df4e554308f2b67c7d4d1a902d3d666a757e3
used2bgood <jarenbanks@gmail.com>
2025-08-08 20:12:35 -0500

    Create .eleventy.js

---
+++ .eleventy.js
@@ -0,0 +1,4 @@
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy({ "src/assets": "assets" });
+  return { dir: { input: "src", includes: "_includes", data: "_data", output: "_site" } };
+};
